9c56ef57cb6c8194ce460af21edd0e1331ac573f
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-01-25 19:39:12 +0600

    Practice Task

---
+++ JS Array/Practice Task/task2.js
@@ -0,0 +1,15 @@
+/*
+2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+Display the final array as output
+*/
+
+
+const dest = ['Kuyakata', 'Rangamati', 'Bandorbon'];
+dest.push('Shitakundo');
+dest.push('SaintMartin', 'Sajek');
+dest.pop();
+console.log(dest);
---
+++ JS Array/Practice Task/task5.js
@@ -0,0 +1,18 @@
+/*
+5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+*/
+
+
+const fruits = ['apple', 'banana', 'mango', 'papaya', 'coconut'];
+console.log(fruits);
+
+const flowers = ['rose', 'lily', 'lotus', 'sunflower'];
+console.log(flowers);
+
+let concat = fruits.concat(flowers);
+console.log(concat);
