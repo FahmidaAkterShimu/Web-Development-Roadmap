e59aaf04041eabeda23949cca19ba7f6cd2d4091
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-01 22:07:53 +0600

    parameter.js

---
+++ JS Function/parameter.js
@@ -0,0 +1,20 @@
+// Parameter
+function square(number) {
+    console.log("Value of the number parameter:", number)
+    const borgo = number * number;
+    console.log("Square of the given number:", borgo);
+}
+
+square(6);
+console.log("-----------------------------------");
+square(12);
+console.log("-----------------------------------");
+
+// Add function
+function add(num1, num2) {
+    console.log("Value of two parameters:", num1, ",", num2);
+    const sum = num1 + num2;
+    console.log("Addition of two given numbers", sum);
+}
+
+add(4, 56);
