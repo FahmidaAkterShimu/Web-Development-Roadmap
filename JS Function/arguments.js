3493b1cda355d53ae9fbff928288a754f2b73b04
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-01 22:08:24 +0600

    arguments.js

---
+++ JS Function/arguments.js
@@ -0,0 +1,17 @@
+function double(number){
+    const double = 2 * number;
+    console.log(number, double);
+}
+
+console.log("Call the function");
+double(14);
+console.log("-----------------");
+double(16);
+console.log("-----------------");
+
+const number = 43;
+double(number);
+console.log("-----------------");
+
+const money = 76;
+double(money);
