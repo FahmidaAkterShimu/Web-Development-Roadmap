6164b3ca09aedfa2111da26c677946a4edbde99d
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-01 22:11:05 +0600

    array-operation

---
+++ JS Function/array-operation.js
@@ -0,0 +1,11 @@
+// Write a function to give me the sum of all numbers in an array
+
+function sumElements(numbers) {
+    let sum = 0;
+    for (number of numbers) {
+        sum = sum + number;
+    }
+    return sum;
+}
+
+console.log("Sum of number is", sumElements([45, 23, 66, 90]));
