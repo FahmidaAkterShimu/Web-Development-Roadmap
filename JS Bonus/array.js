295376f0449b1f923e70a83d23864958957a36e4
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-09 18:46:57 +0600

    array.js

--- JS Bonus/array.js
+++ JS Bonus/array.js
@@ -2,3 +2,10 @@ const products = [
     {name: 'phone', price: 1200},
     {name: 'laptop', price: 32000},
 ]
+
+// Array like object
+const product = {
+    '0': phone,
+    '1': laptop,
+    '2': pc
+}
