a66395a4de1baf7add6bca98cdfdebb723be3dd0
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-26 21:12:25 +0600

    keys-values.js

---
+++ ES6-Intro/keys-values.js
@@ -0,0 +1,6 @@
+const king = { name: 'Musa', age: 55, kingdom: 'pride lands' };
+const keys = Object.keys(king);
+console.log(keys);
+console.log(Object.values(king));
+const entries = Object.entries(king);
+console.log(entries);
