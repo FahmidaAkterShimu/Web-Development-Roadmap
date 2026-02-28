b6524844f30844d420bf9f4004b3659c1e99f852
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-28 17:55:59 +0600

    primitive.js

---
+++ JS Core Concepts/primitive.js
@@ -0,0 +1,24 @@
+let data = 4;
+console.log(typeof data);
+
+data = "something"
+console.log(typeof data);
+
+data = false;
+console.log(typeof data);
+
+data = { price: 22 };
+console.log(typeof data);
+
+data = [];
+console.log(typeof data);
+
+// primitive data types (basic type)
+let a = 4;
+let b = "abc";
+let c = false;
+
+// Non-primitive data types (reference type)
+let d = { price: 22 };
+let e = [34, 56, 4];
+
