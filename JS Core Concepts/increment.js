e2474200735a8454cc06b0aceb9bb8359eb0108d
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-02 17:02:17 +0600

    increment

---
+++ JS Core Concepts/increment.js
@@ -0,0 +1,15 @@
+let a = 5;
+a++;
+console.log(a++);   // age print kore pore increment kore
+console.log(a);
+
+let b = 5;
+b++;
+console.log(++b);   // age increment kore pore print kore
+console.log(b);
+
+
+let c = 5;
+c--;
+console.log(--c);   // age decrement kore pore print kore
+console.log(c--);
