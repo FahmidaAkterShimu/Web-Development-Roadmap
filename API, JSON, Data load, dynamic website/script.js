367837a7d795fee0318cca9b4410e3a9af2392f1
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-03 11:17:57 +0600

    created element into displayPosts and shown the posts as a list

--- API, JSON, Data load, dynamic website/script.js
+++ API, JSON, Data load, dynamic website/script.js
@@ -7,18 +7,3 @@ const loadData = () => {
         .then((data) => console.log(data));
 };
 
-const loadPost = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    fetch(url)
-        .then((res) => res.json())
-        .then(json => {
-            console.log(json);
-            displayPost(json);
-        });
-};
-
-const displayPost = (posts) => {
-    posts.forEach(post => {
-        console.log(post);
-    })
-};
