1e2aab19c0b15368ea64d32a11ccc24fce9f9f04
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-03 20:14:43 +0600

    params ,create dynamic path and render Dynamic Route

--- intro-to-react-router/src/main.jsx
+++ intro-to-react-router/src/main.jsx
@@ -10,6 +10,7 @@ import Mobiles from './components/Mobiles/Mobiles.jsx'
 import Laptops from './components/Laptops/Laptops.jsx';
 import Users from './components/Users/Users.jsx';
 import Users2 from './components/Users2/Users2.jsx';
+import UserDetails from './components/UserDetails/UserDetails.jsx';
 
 const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
@@ -31,6 +32,11 @@ const router = createBrowserRouter([
         element: <Suspense fallback={<span>Loading....</span>}>
           <Users2 usersPromise={usersPromise}></Users2>
         </Suspense>
+      },
+      {
+        path: 'users/:userId',
+        // loader: ({ params }) => fetch('https://jsonplaceholder.typicode.com/users'),
+        Component: UserDetails
       }
     ]
   },
