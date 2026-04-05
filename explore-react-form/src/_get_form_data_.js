6bcd590d8d1a98eed5dbacdbcc7e6adedc0b8369
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-05 12:32:04 +0600

    controlled and uncontrolled ways to collect form data

--- explore-react-form/src/_get_form_data_.js
+++ explore-react-form/src/_get_form_data_.js
@@ -1,2 +1,14 @@
 // 1. e.target.[name of the input field].value
 // 2. use form action and formData in the action handler. formData.get('name of the input field')
+// 3. controlled component. one per each field. use state on change of the field. usefull to dynamically handle error.
+
+// or
+
+//3. handle all controlled field on one state object
+// const [formData, setFormData] = useState({
+//     name: '',
+//     password: '',
+//     phone: '',
+// }) 
+
+// 4. Uncontrolled using useRef
