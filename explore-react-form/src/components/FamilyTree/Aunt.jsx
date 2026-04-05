21cbca9047a4ae14b7178f49cbff5411b4791561
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-05 23:57:03 +0600

    Explore more context API

--- explore-react-form/src/components/FamilyTree/Aunt.jsx
+++ explore-react-form/src/components/FamilyTree/Aunt.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { use } from 'react';
 import Cousin from './Cousin';
+import { MoneyContext } from './FamilyTree';
 
 const Aunt = ({ asset }) => {
+    const [money, setMoney] = use(MoneyContext)
+
+    const handleAddMoney = () => {
+        setMoney(money + 5000);
+    }
+
     return (
         <div>
             <h3>Aunt</h3>
@@ -9,6 +16,7 @@ const Aunt = ({ asset }) => {
                 <Cousin asset={asset} name='Tom'></Cousin>
                 <Cousin name='Jerry'></Cousin>
             </section>
+            <button onClick={handleAddMoney}>Add 5000 tk</button>
         </div>
     );
 };
