96088a8fa20c5f8879d971304b69099d98280565
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-09 17:44:10 +0600

    Create pages, routing in Next.js, and navigation in Next.js

---
+++ my-next-app/src/app/about/designers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesignersPage = () => {
+    return (
+        <div>
+            <h2 className='text-pink-300 text-4xl'>Colorful Designers</h2>
+        </div>
+    );
+};
+
+export default DesignersPage;
