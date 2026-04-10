348e17cbc52cb970f35ae7e03773284138cd8ac5
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-10 17:14:27 +0600

    Image Optimization in Next.js

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -2,6 +2,30 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+
+  images: {
+    remotePatterns: [
+      // https://plus.unsplash.com/premium_photo-1774271492663-a7d50adbed56
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+
+      //  https://i.ibb.co.com/76Yt4VX/Babar-Azam.png
+
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- my-next-app/src/app/about/page.jsx
+++ my-next-app/src/app/about/page.jsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
+import cover from '../../assets/images/cover.jpg'
 
 const AboutPage = () => {
     return (
         <div>
             <h2>This is about page</h2>
+            
+            <Image width="500" height="500" src='https://i.ibb.co.com/76Yt4VX/Babar-Azam.png' alt=''>
+
+            </Image>
+
+            <Image width="500" height="500" src="https://plus.unsplash.com/premium_photo-1774271492663-a7d50adbed56" alt=""></Image>
+
+            <Image width="500" height="500" src='/cover.jpg' alt="cover pic"></Image>
+
+            <Image wisth="300" height="500" src={cover} alt="same img"></Image>
         </div>
     );
 };
