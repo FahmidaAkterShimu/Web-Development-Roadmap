d4d1e03ec1442b229d196fa0d20a262c8ff9ab7b
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-30 23:14:45 +0600

    Revalidation after server action to display form data

--- next-hero-server-action/src/lib/actions.js
+++ next-hero-server-action/src/lib/actions.js
@@ -1,5 +1,24 @@
-export const createATask = async(formData) => {
-'use server'
-const name = formData.get('name');
-console.log(formData);
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
+
+export const createATask = async (formData) => {
+    'use server'
+    // const title = formData.get('title');
+    // const description = formData.get('description');
+    // const status = formData.get('status');
+    // const priority = formData.get('priority');
+    // const assignedTo = formData.get('assignedTo');
+
+    // const newTask = {title, description, status, priority, assignedTo}
+
+    const newTask = Object.fromEntries(formData.entries());
+
+
+    console.log(newTask);
+
+    const res = await postTask(newTask);
+    if (res.ok) {
+        revalidatePath('/tasks');
+    }
+    return res;
 }
