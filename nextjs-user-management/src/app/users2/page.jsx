444198d88697c4300b4768f30657dc6fca214b11
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-07-19 16:13:23 +0600

    NextJs user management

---
+++ nextjs-user-management/src/app/users2/page.jsx
@@ -0,0 +1,23 @@
+import UsersList from "@/components/UsersList";
+import { Suspense } from "react";
+
+// const usersPromise = fetch('').then(res => res.json());
+
+const getUsers = async () => {
+    const res = await fetch('http://localhost:8000/users');
+    return res.json();
+}
+
+const Users2Page = () => {
+    const userPromise = getUsers();
+    return (
+        <div>
+            <h2>Users2: with suspense</h2>
+            <Suspense fallback={<div>Loading...</div>}>
+                <UsersList usersPromise={userPromise}></UsersList>
+            </Suspense>
+        </div>
+    );
+};
+
+export default Users2Page;
